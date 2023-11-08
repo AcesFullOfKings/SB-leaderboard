7aa12a83c39a61400fdae9e027dea28323cf11d6
AcesFullOfKings <6105690+AcesFullOfKings@users.noreply.github.com>
2023-11-08 15:32:52 +0000

    Update generate_leaderboard.py
    new memory-efficient design, userID added to csv, and more optimisations

--- generate_leaderboard.py
+++ generate_leaderboard.py
@@ -8,54 +8,70 @@ usernames.csv:    userID,userName,locked
 				  315k rows
 """
 
-sponsortimes = open("sponsorTimes.csv", "r")
-first_row = sponsortimes.readline() # discard this so it's not processed int he loop below
+import csv
+from time import time
+
+start_time=time()
+
+# set up segs reader:
+sponsortimes = open("download/sponsorTimes.csv", "r")
+first_row = sponsortimes.readline() # discard this so it's not processed in the loop below
+del first_row
+
+sponsortimes_field_names = ["videoID","startTime","endTime","votes","locked","incorrectVotes","UUID","userID","timeSubmitted","views","category","actionType","service","videoDuration","hidden","reputation","shadowHidden","hashedVideoID","userAgent","description"]
+segment_reader = csv.DictReader(sponsortimes, fieldnames=sponsortimes_field_names)
+
+
+#set up users reader:
+usernames = open("download/userNames.csv", "r")
+first_row = usernames.readline() # discard this so it's not processed in the loop below
+del first_row
+
+usernames_field_names = ["userID","userName","locked"]
+username_reader = csv.DictReader(usernames, fieldnames=usernames_field_names)
 
 users = dict() # format -> {userID:{username:"", submissions:0, skips:0, time_saved:0},}
-
 line_num = 0
 
 print("Processing sponsorTimes.csv..")
-for line in sponsortimes:
-	try:
-		videoID,startTime,endTime,votes,locked,incorrectVotes,UUID,userID,timeSubmitted,views,category,actionType,service,videoDuration,hidden,reputation,shadowHidden,hashedVideoID,userAgent,description = line.split(",")
-	except:
-		#print(f"ignoring broken line on line {line_num}")
-		#print(line)
-		continue
+
+for segment in segment_reader:
+	votes        = int(segment["votes"])
+	hidden       = int(segment["hidden"])
+	shadowHidden = int(segment["shadowHidden"])
+	endTime      = float(segment["endTime"])
+	startTime    = float(segment["startTime"])
+	views        = int(segment["views"])
+	userID       = segment["userID"]
+	actionType   = segment["actionType"]
 
 	# don't count removed/hidden submissions
-	if ((int(votes) > -2) and (hidden == "0") and (shadowHidden == "0") and 
-		(actionType=="skip")):
-		duration = float(endTime)-float(startTime)
-		views = int(views)
+	if ((votes > -2) and (not hidden) and (not shadowHidden) and (actionType=="skip")):
+		duration = endTime-startTime
 
 		if userID not in users:
 			users[userID] = {"submissions":0, "total_skips":0, "time_saved":0, "username":userID}
 
 		users[userID]["total_skips"] += views
-		users[userID]["time_saved"] += (duration*views)
+		users[userID]["time_saved"]  += (duration*views)
 		users[userID]["submissions"] += 1
 
 	line_num += 1
 	if not line_num%1000000:
 		print(f"Processing line {line_num}")
 
+sponsortimes.close() # finished with this file now
+end_time=time()
 
+print(f"Time taken: {round(end_time-start_time,1)}")
 print("Processing usernames.csv..")
-usernames = open("usernames.csv", "r")
 
-line_num=0
+line_num=0 # only for printing progress
 
-for row in usernames:
-	try:
-		columns   = row.split(",")
-		userID    = columns[0]
-		locked    = columns[-1]
-		user_name = ",".join(columns[1:-1]) # bc sometimes the username contains commas which is a headache
-		user_name = user_name.strip("\"")
-	except ValueError:
-		continue
+for user_row in username_reader:
+	userID    = user_row["userID"]
+	user_name = user_row["userName"]
+	#locked    = user_row["locked"]
 
 	if userID in users:
 		users[userID]["username"] = user_name
@@ -65,13 +81,22 @@ for row in usernames:
 	if not line_num%100000:
 		print(f"Processing user {line_num}")
 
-#convert dict to list of lists (for easier sorting)
-user_list = [[user_id, user_info["username"], user_info["submissions"], user_info["total_skips"], user_info["time_saved"]]
-             for user_id, user_info in users.items()]
+usernames.close() # finished with this
 
-top_skips       = sorted(user_list, key=lambda x: x[2], reverse=True)[:200]
-top_submissions = sorted(user_list, key=lambda x: x[3], reverse=True)[:200]
+#convert dict to list of lists (for easier sorting below)
+user_list=[]
+while users:
+	user_id, user_info = users.popitem() # returns and deletes the next item from users as a tuple of (key, value)
+	user_tuple = (user_id, user_info["username"], user_info["submissions"], user_info["total_skips"], user_info["time_saved"])
+	user_list.append(user_tuple)
+
+del users
+
+top_submissions = sorted(user_list, key=lambda x: x[2], reverse=True)[:200]
+top_skips       = sorted(user_list, key=lambda x: x[3], reverse=True)[:200]
 top_time_saved  = sorted(user_list, key=lambda x: x[4], reverse=True)[:200]
+
+del user_list
 
 # Merge the users from each top list
 added_userIDs = set() # so we don't add a user more than once
@@ -97,18 +122,17 @@ line_num = 0
 
 with open("leaderboard.csv", "w") as f:
 	for user in top_users:
-		username    = user[1]
+		user_id     = user[0]
+		username    = user[1].replace(",", "") # no commas, bad
 		submissions = user[2]
 		skips       = user[3]
 		time_saved  = round(user[4])
 
-		f.write(f"{username}, {submissions}, {skips}, {time_saved}\n")
+		f.write(f"{user_id},{username},{submissions},{skips},{time_saved}\n")
 
 		line_num += 1
 		
 		if not line_num%100:
 			print(f"Writing line {line_num}")
 
-		#for testing: 
-		#if line_num > 100_000:
-		#	break
+print("Done!")
